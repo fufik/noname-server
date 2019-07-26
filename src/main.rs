13e3cd71b3c61221f36636ace69833fa2c149487
fufik <ivaiva1999ivaiva@gmail.com>
2019-07-26 15:06:02 +0000

    Refactor handle_connection

--- src/main.rs
+++ src/main.rs
@@ -14,51 +14,69 @@ fn main() {
     }
 }
 
+
 fn handle_connection(mut stream: TcpStream){
-    let re = Regex::new(r"GET (?P<address>([0-9A-Za-z]|/|\.|_)+)\??(?P<params>([0-9A-Za-z]|=|\+|%|:|-|_)+)? ").unwrap();
     let mut buffer = [0;512];
-    let mut contents = String::new();
-    let (mut address,mut params,mut code) = ("/","","200 OK");
     stream.read(&mut buffer).unwrap();
-    let buffer = String::from_utf8_lossy(&buffer);
-    let capture = &re.captures(&buffer);
-    match capture{
-        Some(a) => {
-                    address = match a.name("address"){
-                        Some(b) => b.as_str(),
-                        None    => "/",
-                    };
-                    params  = match a.name("params"){
-                        Some(b) => b.as_str(),
-                        None    => "",
-                    };
-        },
-        None    =>  {
-                     println!("400 Bad request");
-                     code="400 Bad request";
-        },
-    }
-    println!("{}",address);
-    if address == "/"{
-        contents = fs::read_to_string("index.html").unwrap();
+
+    let mut answer = move |Code: (u16,&str), contents: String|{
+        let response = format!("HTTP/1.1 {} {}\r\n\r\n{}", Code.0,Code.1, contents);
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
+    };
+
+    let mut err_answer = |Code: (u16,&str)|{
+        let contents = fs::read_to_string(format!("errors/{}.html",Code.0)).unwrap();
+        answer(Code,contents);
+    };
+
+    if !buffer.starts_with(b"GET")
+    {
+        //...Something I don't get, 
+        err_answer((400,"Bad request"));
+        return;
     } else {
-        let fname = &address[1..];
-        //println!("{}",fname);
-        let a = fs::read_to_string(fname);
-        match a{
-            Ok(n)  => contents = n,
-            Err(_) => {
-                       println!("404 Not Found");
-                       code = "404 Not Found";
+        let re = Regex::new(r"GET (?P<address>([0-9A-Za-z]|/|\.|_)+)\??(?P<params>([0-9A-Za-z]|=|\+|%|:|-|_)+)? ").unwrap();
+        let buffer = String::from_utf8_lossy(&buffer);
+        let capture = &re.captures(&buffer);
+        let (mut address,mut params) = ("/","");
+        match capture{
+            Some(a) => {
+                address = match a.name("address"){
+                    Some(b) => b.as_str(),
+                    None    => "/",
+                };
+                params  = match a.name("params"){
+                    Some(b) => b.as_str(),
+                    None    => "",
+                };
+            },
+            None    =>  {
+                println!("400 Bad request");
+                err_answer((400,"Bad request"));
+                return;
             },
         }
+        
+        let mut contents = String::new();
+        if address == "/"
+        {
+            contents = fs::read_to_string("index.html").unwrap();
+        } else {
+            let fname = &address[1..];
+            //println!("{}",fname);
+            let a = fs::read_to_string(fname);
+            match a{
+                Ok(n)  => contents = n,
+                Err(_) => {
+                    println!("404 Not Found");
+                    err_answer((404,"Not found"));
+                    return;
+                },
+            }
+        }
+        answer((200,"OK"),contents);
     }
-    if code != "200 OK"{
-        let a = &code[0..3];
-        contents = fs::read_to_string(format!("errors/{}.html",a)).unwrap();
-    }
-    let response = format!("HTTP/1.1 {}\r\n\r\n{}", code, contents);
-    stream.write(response.as_bytes()).unwrap();
-    stream.flush().unwrap();
-    //println!("Request: {}",String::from_utf8_lossy(&buffer[..]));
+    
+
 }
